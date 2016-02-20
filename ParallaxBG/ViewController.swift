e386c2408fc2ff5471652f1c5ae24c8d45e8e152
Chris Sprague <chris@chaoscontrol.org>
2016-02-20 08:26:05 -0500

    Initial Commit

---
+++ ParallaxBG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxBG
+//
+//  Created by Chris Sprague on 2/20/16.
+//  Copyright © 2016 2DSquirrel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
