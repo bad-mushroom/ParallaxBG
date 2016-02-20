425b36b0bc64f9fcad2d6f31b25f306920e86a17
Chris Sprague <chris@chaoscontrol.org>
2016-02-20 09:03:26 -0500

    Load scene

---
+++ ParallaxBG/ParallaxScene.swift
@@ -0,0 +1,21 @@
+//
+//  ParallaxScene.swift
+//  ParallaxBG
+//
+//  Created by Chris Sprague on 2/20/16.
+//  Copyright © 2016 2DSquirrel. All rights reserved.
+//
+
+import SpriteKit
+
+class ParallaxScene: SKScene
+{
+    override func didMoveToView(view: SKView)
+    {
+    }
+    
+    override func update(currentTime: CFTimeInterval)
+    {
+    }
+    
+}
--- ParallaxBG/ViewController.swift
+++ ParallaxBG/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import SpriteKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let parallaxScene = ParallaxScene(size: view.bounds.size)
+        let parallaxSceneView = view as! SKView
+        
+        parallaxSceneView.showsFPS = true
+        parallaxSceneView.showsNodeCount = true
+        parallaxSceneView.ignoresSiblingOrder = true
+        parallaxScene.scaleMode = .AspectFill
+        
+        parallaxSceneView.presentScene(parallaxScene)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
